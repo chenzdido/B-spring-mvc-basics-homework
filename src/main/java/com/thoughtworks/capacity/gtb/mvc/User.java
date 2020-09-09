5f39c9f7c8fe48776560685f8d87da6887d4cd7e
chenzdido <chenzdido@gmail.com>
2020-09-09 23:59:59 +0800

    [zhe] feat:add id.

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/User.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/User.java
@@ -8,7 +8,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    private static Integer count=0;
+    private Integer id;
     private String username;
     private String password;
     private String email;
+
+    User(String username,String password,String email){
+        id=++count;
+        this.username=username;
+        this.password=password;
+        this.email=email;
+    }
 }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.capacity.gtb.mvc;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class UserService {
+
+    private Map<Integer, User> userMap=new HashMap<>();
+
+    public UserService(){
+        userMap.put(1, new User("Tom","12345","tom@qq.com"));
+    }
+
+    public void createUser(User user){
+        userMap.put(user.getId(),user);
+    }
 }
